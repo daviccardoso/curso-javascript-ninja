17e8cb2b53301d075c0095e97ccd44d462cd5e7d
Davi Cardoso <daviccardoso@users.noreply.github.com>
2018-06-09 02:07:49 -0300

    Resolução de desafios da semana 5

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,22 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+
+var arr = ['Davi', 26, ['JS', 'SQL', 'SAP DS'], function () { }, { propriedade: 'valor' }];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+
+function retornarArray(arr) {
+    return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+
+console.log(retornarArray(arr)[1]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +25,23 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+
+function retornarIndice(arr, index) {
+    return arr[index];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+
+var meuArray = [2.2, true, undefined, 'valor_alfa', NaN];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+
+console.log(retornarIndice(meuArray, 3));
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +57,55 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+
+function book(title) {
+    var books = {
+        'Conhecendo as Doutrinas da Bíblia': {
+            quantidadePaginas: 409,
+            autor: 'Myer Pearlman',
+            editora: 'Vida'
+        },
+        'A Interpretação Bíblica': {
+            quantidadePaginas: 360,
+            autor: 'Roy Zuck',
+            editora: 'Vida Nova'
+        },
+        'Em Guarda': {
+            quantidadePaginas: 320,
+            autor: 'William Lane Craig',
+            editora: 'Vida Nova'
+        }
+    };
+
+    return title === undefined ? books : books[title];
+}
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+
+console.log(book());
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+var title = 'Conhecendo as Doutrinas da Bíblia';
+console.log('O livro "' + title + '" tem ' + book(title).quantidadePaginas + ' páginas!');
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+
+console.log('O autor do livro "' + title +'" é ' + book(title).autor + '.');
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+
+console.log('O livro "' + title + '" foi publicado pela editora '+ book(title).editora + '.');
